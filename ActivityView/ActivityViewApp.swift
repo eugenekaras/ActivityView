acb8fa6a8b8dcb8da176b11d2b7f49de2ab70b8b
ekaras <key.cpp@gmail.com>
2022-12-13 02:03:56 +0300

    1

---
+++ ActivityView/ActivityViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityViewApp.swift
+//  ActivityView
+//
+//  Created by Евгений Карась on 13.12.22.
+//
+
+import SwiftUI
+
+@main
+struct ActivityViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
